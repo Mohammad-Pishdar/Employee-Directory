b87e41f589ff4338be646fd2c0f10d0007eebcd3
Mo Pishdar <mohpish@gmail.com>
2020-09-28 09:11:43 +1000

    added the app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useEffect, useState } from "react";
+import Filter from "./Filter";
+import Table from "./components/Table";
+import { getEmployees } from "./utils/API";
 
 function App() {
-  return <PortfolioContainer />;
+  const [initialEmployees, updateAvailableEmployees] = useState([]);
+  const [employeesToRender, updateEmployeesToRender] = useState([]);
+
+  useEffect(() => {
+    getEmployees().then(({ data: { results } }) => updateAvailableEmployees(results));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Employee Directory</h1>
+      <p>
+        Please enter the employees last name to narrow down your search results
+      </p>
+      <FilterInput employees={initialEmployees} updateEmployees={updateEmployeesToRender} />
+      <Table users={employeesToRender} />
+    </div>
+  );
 }
 
 export default App;
