211232c0393748ccb2eb98f1f2f05894eda65747
Mo Pishdar <mohpish@gmail.com>
2020-09-28 13:25:44 +1000

    ready to deploy

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//Getting a list of random people in Australia from random user API to populate our table
+
 export const getEmployees = function() {
   return axios.get("https://randomuser.me/api/?results=200&nat=au");
 };
