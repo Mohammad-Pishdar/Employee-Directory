6ca59c2624dffbfe8494ce1d4b144c2927db3806
Mo Pishdar <mohpish@gmail.com>
2020-09-28 16:25:06 +1000

    Updates

---
+++ precache-manifest.682e692d2f6c8484cb8ed15f6b3a1e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b42c06b1dcea986ce113bc818973cbc",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "685f4fa32523fd11c27d",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2c2c00e1f7969bd47f0b",
+    "url": "/Employee-Directory/static/css/main.2a4ecd36.chunk.css"
+  },
+  {
+    "revision": "685f4fa32523fd11c27d",
+    "url": "/Employee-Directory/static/js/2.49c994df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.49c994df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2c00e1f7969bd47f0b",
+    "url": "/Employee-Directory/static/js/main.02e6c579.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
